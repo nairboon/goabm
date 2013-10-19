7bb3200bf49bd672bb3df220e3e2b7ea80b129c1
Remo Hertig <remo.hertig@bluewin.ch>
2013-10-19 23:53:33 +0200

    added model with moving agents

---
+++ examples/axelrod_moving/abm_axelrod_moving.go
@@ -0,0 +1,162 @@
+/*
+This file is part of GoABM
+Copyright 2013 by Remo Hertig <remo.hertig@bluewin.ch>
+*/
+
+// An extension to Robert Axelrods ABM model of disseminating culture [1]
+// [1]: Axelrod, Robert. "The dissemination of culture a model with local convergence and global polarization." Journal of conflict resolution 41, no. 2 (1997): 203-226.
+// based on the work of Arezky Hernandez Rodriguez
+// (Overview , Design Concepts , and Details ( ODD ) for the Axelrod ’ s model for Cultural Dissemination)
+//
+// In this model agents will move around the landscape
+package main
+
+import "fmt"
+import "math/rand"
+import "goabm"
+import "flag"
+
+// Implementation of the Agent, cultural Traits are stored in features
+type AxelrodAgent struct {
+	features Feature
+	Agent    goabm.FLWMAgenter
+	Steplength float64
+	ProbVeloc float64
+}
+
+// returns the culture as a string
+func (a *AxelrodAgent) Culture() string {
+	return fmt.Sprintf("%v", a.features)
+}
+
+// required for the simulation interface, called everytime when the agent is activated
+func (a *AxelrodAgent) Act() {
+	//fmt.Printf("Agent culture: %v\n",a.features)
+
+
+	dice := rand.Float64()
+	// (i) agent decides to move according to the probability veloc
+	if dice <= a.ProbVeloc {
+		a.Agent.MoveRandomly(a.Steplength)
+	}
+
+	// (ii) (a) selects a neighbor for cultural interaction
+	res := a.Agent.GetRandomNeighbor()
+	// there is no agent in sight
+	if res == nil {
+	return
+	}
+	other :=res.(*AxelrodAgent)
+	sim := a.Similarity(other)
+	if sim >= 0.99 {
+		// agents are already equal
+		return
+	}
+	dice2 := rand.Float32()
+	//fmt.Printf("interacting %f <= %f\n",probabilityToInteract,sim)
+	//interact with sim% chance
+	if dice2 <= sim {
+
+		for i := range a.features {
+			if a.features[i] != other.features[i] {
+				//fmt.Printf("%d influenced %d\n", other.seqnr, a.seqnr)
+				a.features[i] = other.features[i]
+				return
+			}
+
+		}
+	}
+
+}
+
+// helper function to determine the similarity between to agents
+func (a *AxelrodAgent) Similarity(other *AxelrodAgent) float32 {
+	c := float32(0.0)
+	// count equal traits, final score = shared traits/total traits
+	for i := range a.features {
+		if a.features[i] == other.features[i] {
+			c = c + 1
+		}
+	}
+	//fmt.Printf("sim: %f/%d\n",c,len(a.features))
+	return c / float32(len(a.features))
+}
+
+type Feature []int
+// implementation of the model
+type Axelrod struct {
+	Cultures  int
+	Landscape goabm.Landscaper
+	Traits    int
+	Features  int
+	Steplength float64
+	ProbVeloc float64
+}
+
+func (a *Axelrod) Init(l goabm.Landscaper) {
+	a.Landscape = l
+}
+
+func (a *Axelrod) CreateAgent(agenter interface{}) goabm.Agenter {
+
+	agent := &AxelrodAgent{Agent: agenter.(goabm.FLWMAgenter)}
+
+	f := make(Feature, a.Features)
+	for i := range f {
+		f[i] = rand.Intn(a.Traits)
+	}
+	agent.features = f
+	agent.ProbVeloc = a.ProbVeloc
+	agent.Steplength = a.Steplength
+	return agent
+}
+
+func (a *Axelrod) LandscapeAction() {
+	a.Cultures = a.CountCultures()
+
+}
+
+func (a *Axelrod) CountCultures() int {
+	cultures := make(map[string]int)
+	for _, b := range a.Landscape.GetAgents() {
+		a := b.(*AxelrodAgent)
+		cul := a.Culture()
+		if _, ok := cultures[cul]; ok {
+			cultures[cul] = 1
+		} else {
+			cultures[cul] = cultures[cul] + 1
+		}
+	}
+	return len(cultures)
+}
+
+func main() {
+	fmt.Println("ABM simulation")
+	// general model parameters
+	var traits = flag.Int("traits", 5, "number of cultural traits per feature")
+	var features = flag.Int("features", 5, "number of cultural features")
+	var size = flag.Int("size", 10, "size (width/height) of the landscape")
+
+	// parameters for the moving model
+	var probveloc = flag.Float64("pveloc", 0.05, "probability that an agent moves")
+	var steplength = flag.Float64("steplength", 0.1, "maximal distance a agent can travel per step")
+	var sight = flag.Float64("sight", 1, "radius in which agent can interact")
+	var numAgents = flag.Int("agents", 100, "number of agents to simulate")
+
+	var runs = flag.Int("runs", 200, "number of simulation runs")
+	flag.Parse()
+
+	model := &Axelrod{Traits: *traits, Features: *features, ProbVeloc: *probveloc, Steplength: *steplength}
+	sim := &goabm.Simulation{Landscape: &goabm.FixedLandscapeWithMovement{Size: *size, NAgents: *numAgents,Sight:*sight}, Model: model}
+	sim.Init()
+	for i := 0; i < *runs; i++ {
+		fmt.Printf("Step #%d, Events:%d, Cultures:%d\n", i, sim.Stats.Events, model.Cultures)
+		if model.Cultures == 1 {
+			return
+		}
+		sim.Step()
+
+	}
+	//fmt.Printf("%v\n",sim.Landscape.GetAgents())
+
+}
---
+++ flwm.go
@@ -0,0 +1,97 @@
+/*
+This file is part of GoABM
+Copyright 2013 by Remo Hertig <remo.hertig@bluewin.ch>
+*/
+package goabm
+
+import "fmt"
+import "math/rand"
+import qt "github.com/larspensjo/quadtree" 
+import vector "github.com/proxypoke/vector" 
+
+// 2d landscape with movement
+type FixedLandscapeWithMovement struct {
+	Agents     []FLWMAgent // library agent object, implements neighbor selection etc.
+	UserAgents []Agenter   // agents from the user
+	Size       int
+	Sight	   float64
+	NAgents    int
+	width      int
+	height     int
+	tree	   *qt.Quadtree
+}
+
+type FLWMAgenter interface {
+	GetRandomNeighbor() Agenter
+	MoveRandomly(float64)
+}
+
+type FLWMAgent struct {
+	seqnr int
+	x     float64
+	y     float64
+	ls    *FixedLandscapeWithMovement
+	qt.Handle
+	//exe Agenter
+}
+
+func (l *FixedLandscapeWithMovement) GetAgents() []Agenter {
+
+	return l.UserAgents
+}
+
+
+func (a *FLWMAgent) MoveRandomly(steplength float64) {
+	//random direction
+	v:= vector.NewFrom([]float64{rand.Float64(),rand.Float64()})
+	v.Normalize()
+	v.Scale(steplength)
+	x,_ := v.Get(0)
+	y,_ := v.Get(1)
+	a.ls.tree.Move(a,qt.Twof{x,y})
+}
+
+func (a *FLWMAgent) GetRandomNeighbor() Agenter {
+	possibleNeighbors := a.ls.tree.FindNearObjects(qt.Twof{a.x, a.y},a.ls.Sight)
+	if len(possibleNeighbors) < 1 {
+	  return nil
+}
+	choice := rand.Int31n(int32(len(possibleNeighbors)))
+	i:= possibleNeighbors[choice].(*FLWMAgent).seqnr
+	return a.ls.UserAgents[i]
+}
+
+func (l *FixedLandscapeWithMovement) Init(model Modeler) {
+	numAgents := l.Size * l.Size
+	fmt.Printf("Init landscape with %d agents\n", numAgents)
+
+	l.width = l.Size
+	l.height = l.Size
+	
+	l.tree = qt.MakeQuadtree(qt.Twof{0, 0}, qt.Twof{float64(l.Size), float64(l.Size)})
+	
+	l.Agents = make([]FLWMAgent, numAgents)
+	l.UserAgents = make([]Agenter, numAgents)
+	y := 0
+	x := 0
+	for i := range l.Agents {
+		//for i:=0;i<numAgents;i++ {
+		l.UserAgents[i] = model.CreateAgent(&l.Agents[i])
+
+		l.Agents[i].seqnr = i
+		l.Agents[i].x = float64(x)
+		l.Agents[i].y = float64(y)
+
+		l.tree.Add(&l.Agents[i],qt.Twof{float64(x), float64(y)})
+
+		x += 1
+		if x >= l.width {
+			// new row
+			x = 0
+			y += 1
+
+		}
+		l.Agents[i].ls = l
+
+	}
+}
